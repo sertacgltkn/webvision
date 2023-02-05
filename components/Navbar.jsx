daa29487b7ac83155b81fb5d273473026160b7ee
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-02-05 17:17:03 +0300

    refactor

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -22,20 +22,24 @@ const Navbar = () => {
       }
     };
     window.addEventListener("scroll", changeColor);
+
+    return () => {
+      window.removeEventListener("scroll", changeColor);
+    };
   }, []);
 
   return (
     <div
-      style={{ backgroundColor: `${color}` }}
-      className="fixed left-0 top-0 w-full z-10 ease-in duration-300"
+      style={{ backgroundColor: color }}
+      className="fixed top-0 left-0 w-full z-10 ease-in duration-300"
     >
-      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+      <div className="max-w-1240 m-auto p-4 flex justify-between items-center">
         <Link href="/">
-          <h1 style={{ color: `${textColor}` }} className="font-bold text-4xl">
+          <h1 style={{ color: textColor }} className="font-bold text-4xl">
             WebVision
           </h1>
         </Link>
-        <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
+        <ul style={{ color: textColor }} className="hidden sm:flex">
           <li className="p-4">
             <Link href="/">Home</Link>
           </li>
@@ -51,29 +55,39 @@ const Navbar = () => {
         </ul>
         <div onClick={handleNav} className="block sm:hidden z-10">
           {nav ? (
-            <AiOutlineClose size={20} style={{ color: `${textColor}` }} />
+            <AiOutlineClose size={20} style={{ color: textColor }} />
           ) : (
             <AiOutlineMenu size={20} />
           )}
         </div>
         <div
-          className={
-            nav
-              ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
-              : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
-          }
+          className={`sm:hidden absolute top-0 left-${
+            nav ? 0 : "-100%"
+          } right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300`}
         >
           <ul>
-            <li onClick={handleNav} className="p-4 text-4xl hover:text-gray-500">
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
               <Link href="/">Home</Link>
             </li>
-            <li onClick={handleNav} className="p-4 text-4xl hover:text-gray-500">
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
               <Link href="/#gallery">Gallery</Link>
             </li>
-            <li onClick={handleNav} className="p-4 text-4xl hover:text-gray-500">
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
               <Link href="/work">Work</Link>
             </li>
-            <li onClick={handleNav} className="p-4 text-4xl hover:text-gray-500">
+            <li
+              onClick={handleNav}
+              className="p-4 text-4xl hover:text-gray-500"
+            >
               <Link href="/contact">Contact</Link>
             </li>
           </ul>
